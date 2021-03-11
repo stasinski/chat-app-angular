a55cd87686352542727e2db7333afdd9828c8ae6
stasinski <01dawids@gmail.com>
2021-03-11 13:06:53 +0100

    init

---
+++ firebase.ts
@@ -0,0 +1,12 @@
+import { AngularFireModule } from '@angular/fire';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDUvGO1G_uypQDOI1loImNclLM5Tl9Qy-Y',
+  authDomain: 'chat-47a03.firebaseapp.com',
+  projectId: 'chat-47a03',
+  storageBucket: 'chat-47a03.appspot.com',
+  messagingSenderId: '102398731173',
+  appId: '1:102398731173:web:b064c84e763ba114913e4c',
+};
+// Initialize Firebase
+AngularFireModule.initializeApp(firebaseConfig);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AngularFirestoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
